ee18f3c4854a5f1924033890db101fabb6998fe7
Will Oliver <willoliver@Wills-Invent-MacBook.local>
2023-12-06 19:36:27 +0000

    first commit

---
+++ src/event-base.cpp
@@ -0,0 +1,81 @@
+#include <memory>
+#include <event2/event.h>
+
+#include "event-base.hpp"
+#include "event.hpp"
+
+using namespace libev;
+
+EventBase::EventBase(): 
+    base { std::unique_ptr<event_base>(event_base_new()) } 
+{}
+
+EventBase::~EventBase() {
+    event_base_free(&(*base));
+}
+
+Event* EventBase::new_event(evutil_socket_t fd, short what, event_callback_fn cb) {
+    events[fd] = new Event(&(*base), fd, what, cb, event_self_cbarg());
+    return events[fd];
+}
+
+Event* EventBase::new_event(evutil_socket_t fd, short what, event_callback_fn cb, void *arg) {
+    events[fd] = new Event(&(*base), fd, what, cb, arg);
+    return events[fd];
+}
+
+int EventBase::run() {
+    return event_base_dispatch(&(*base));
+}
+
+int EventBase::run(bool blocking, bool once, bool exit_on_empty) {
+    int flags = (blocking ? 0 : EVLOOP_NONBLOCK)
+    |(once ? EVLOOP_ONCE : 0)
+    |(exit_on_empty ? 0 : EVLOOP_NO_EXIT_ON_EMPTY);
+    
+    return event_base_loop(&(*base), flags);
+}
+
+bool EventBase::loopexit() {
+    event_base_loopexit(&(*base), nullptr) == 0;
+}
+
+bool EventBase::loopexit(const timeval *tv) {
+    event_base_loopexit(&(*base), tv) == 0;
+}
+
+bool EventBase::loopbreak() {
+    event_base_loopbreak(&(*base)) == 0;
+}
+
+bool EventBase::did_exit() {
+    return event_base_got_exit(&(*base));
+}
+
+bool EventBase::did_break() {
+    return event_base_got_break(&(*base));
+}
+
+bool EventBase::loopcontinue() {
+    return event_base_loopcontinue(&(*base)) == 0;
+}
+
+void EventBase::dump_status() {
+    event_base_dump_events(&(*base), stdout);
+}
+
+void EventBase::dump_status(FILE* fd) {
+    event_base_dump_events(&(*base), fd);
+}
+
+int EventBase::foreach(event_base_foreach_event_cb cb, void* arg) {
+    return event_base_foreach_event(&(*base), cb, arg);
+}
+
+bool EventBase::set_num_priorities(int n) {
+    return event_base_priority_init(&(*base), n) == 0;
+}
+
+Event* EventBase::get_running_event() {
+    return new Event(event_base_get_running_event(&(*base)));
+}
