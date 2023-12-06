17c63b60ab3c73d828d1f339e75b09fadd50ebcc
Will Oliver <willoliver.main@gmail.com>
2023-12-06 20:11:54 +0000

    include cmake build

---
+++ build/CMakeFiles/LibeventPlus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibeventPlus.dir/src/event-base.cpp.o"
+  "CMakeFiles/LibeventPlus.dir/src/event-base.cpp.o.d"
+  "CMakeFiles/LibeventPlus.dir/src/event.cpp.o"
+  "CMakeFiles/LibeventPlus.dir/src/event.cpp.o.d"
+  "libLibeventPlus.a"
+  "libLibeventPlus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibeventPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LibeventPlus.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LibeventPlus.
+# This may be replaced when dependencies are built.
